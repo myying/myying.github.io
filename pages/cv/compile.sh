fcaa4740ba95a84635407ed361cfbc55ee8fb649
Yue Ying <hardyying@gmail.com>
2022-09-12 01:56:24 +0200

    fix cv and add presentation list

--- pages/cv/compile.sh
+++ pages/cv/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+
 ###publication section
 rm publications.tex
 echo "\begin{enumerate}" >> publications.tex
@@ -10,16 +11,16 @@ set -- ../publications/*
 i=$#
 while [ $i -gt 0 ]; do
     eval "item=\${$i}"  ##item in the publication list
-    echo '\item '`pandoc -f html -t latex $item/author`', '`cat $item/year`': '`cat $item/title`'. ' >> publications.tex
-    if [[ `cat $item/status` == 'published' ]]; then
-        echo '\textit{'`cat $item/journal`'}, '`cat $item/issue`', '`cat $item/pages`'. ' >> publications.tex
-        echo '\href{https://doi.org/'`cat $item/doi`'}{doi:'`cat $item/doi`'}.' >> publications.tex
+    echo "\item "`pandoc -f html -t latex $item/author`", "`cat $item/year`": "`cat $item/title`". " >> publications.tex
+    if [[ `cat $item/status` == "published" ]]; then
+        echo "\textit{"`cat $item/journal`"}, "`cat $item/issue`", "`cat $item/pages`". " >> publications.tex
+        echo "\href{https://doi.org/"`cat $item/doi`"}{doi:"`cat $item/doi`"}." >> publications.tex
     fi
-    if [[ `cat $item/status` == 'in review' ]]; then
-        echo '\textit{'`cat $item/journal`'}, in review.' >> publications.tex
+    if [[ `cat $item/status` == "in review" ]]; then
+        echo "\textit{"`cat $item/journal`"}, in review." >> publications.tex
     fi
-    if [[ `cat $item/status` == 'in prep' ]]; then
-        echo 'in prep.' >> publications.tex
+    if [[ `cat $item/status` == "in prep" ]]; then
+        echo "in prep." >> publications.tex
     fi
     echo >> publications.tex
     i=$((i-1))
@@ -31,5 +32,19 @@ echo "\end{enumerate}" >> publications.tex
 ###presentation section
 rm presentations.tex
 echo "\begin{enumerate}" >> presentations.tex
+
+##read list in reverse order (newer first)
+set -- ../presentations/*
+i=$#
+while [ $i -gt 0 ]; do
+    eval "item=\${$i}"  ##date list
+    date=`basename $item`
+    echo "\item "`pandoc -f html -t latex $item/author`", \textit{\`\`"`cat $item/title`"''}, "`cat $item/place`", "`gdate -d $date +'%_d %b, %Y'` >> presentations.tex ###note: gdate works on macos, on linux use date
+    if [ -f $item/invited ]; then
+        echo '(invited)' >> presentations.tex
+    fi
+    echo >> presentations.tex
+    i=$((i-1))
+done
 
 echo "\end{enumerate}" >> presentations.tex
